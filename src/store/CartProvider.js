64b2f176b59cd3d5a9f0c693df0b827ecf397f11
Andrews <allwinandrews@gmail.com>
2021-06-21 15:54:13 +0530

    Adds ADD type to add item in cartReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,38 @@
+import React, { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-export default function CartProvider(props) {
-  const addItemHandler = (item) => {};
+const defaultCartState = { items: [], totalAmount: 0 };
 
-  const removeItemHandler = (id) => {};
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  if (action.type === "REMOVE") return;
+
+  return defaultCartState;
+};
+
+export default function CartProvider(props) {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
